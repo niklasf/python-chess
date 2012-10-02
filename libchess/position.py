77ea63b3bf9dc41d9794941b8a260c782c98f353
Niklas Fiekas <niklas.fiekas@googlemail.com>
2012-10-03 01:55:33 +0200

    Add legal move generator.

--- libchess/position.py
+++ libchess/position.py
@@ -360,6 +360,9 @@ class Position(object):
         invalid en-passant flags are ignored. Players can have more attackers
         on their king than is possible through discovery.
 
+        Methods that require legal move generation and material counting assume
+        the position is valid. If not, their results are undefined.
+
         Raises:
             - Missing black king.
             - Missing white king.
@@ -382,7 +385,7 @@ class Position(object):
             if piece_counts["p"] > 8:
                 raise "Too many %s pawns." % color
 
-            if sum(piece_counts) > 16:
+            if sum(piece_counts.values()) > 16:
                 raise "Too many %s pieces." % color
 
         if self.is_king_attacked("w") and self.is_king_attacked("b"):
@@ -391,6 +394,324 @@ class Position(object):
         if self.is_king_attacked(libchess.opposite_color(self.get_turn())):
             raise "Opposite king in check."
 
+    def is_king_attacked(self, color):
+        """Checks if the king of a player is attacked.
+
+        color: Check if the king of this color is attacked.
+
+        Returns:
+            A boolean indicating whether the king is attacked.
+        """
+        square = self.get_king(color)
+        if square:
+            return self.is_attacked(libchess.opposite_color(color), square)
+        else:
+            return False
+
+    def get_pseudo_legal_moves(self):
+        """Gets pseudo legal moves in the current position.
+
+        Yields: Pseudo legal moves.
+        """
+        PAWN_OFFSETS = {
+            "b": [16, 32, 16, 15],
+            "w": [-16, -32, -17, -15]
+        }
+
+        PIECE_OFFSETS = {
+            KNIGHT: [-18, -33, -31, -14, 18, 33, 31, 14],
+            BISHOP: [-17, -15, 17, 15],
+            ROOK: [-16, 1, 16, -1],
+            QUEEN: [-17, -16, -15, 1, 17, 16, 15, -1],
+            KING: [-17, -16, -15, 1, 17, 16, 15, -1]
+        }
+
+        for square in libchess.Squares.get_all():
+            # Skip pieces of the opponent.
+            piece = self.get(square)
+            if not piece or piece.get_color() != self.get_turn():
+                continue
+
+            # Pawn moves.
+            if piece.get_type() == "p":
+                # Single square ahead. Do not capture.
+                target = libchess.Square.from_0x88_index(square.get_0x88_index() + PAWN_OFFSETS[self.get_turn()][0])
+                if not self.get(target):
+                    # Promotion.
+                    if target.is_backrank():
+                        for promote_to in "bnrq":
+                            yield libchess.Move.get_promotion_move(self, square, target, promote_to)
+
+                    # Two squares ahead. Do not capture.
+                    target = libchess.Square.from_0x88_index(square.get_0x88_index() + PAWN_OFFSETS[self.get_turn()][1])
+                    if (self.get_turn() == "w" and square.get_rank() == 2) or (self.get_turn() == "b" and square.get_rank() == 7) and not board.get(target):
+                        yield libchess.Move.get_big_pawn_move(self, square, target)
+
+                # Pawn captures.
+                for j in [2, 3]:
+                   target_index = square.get_0x88_index() + PAWN_OFFSETS[self.get_turn()][j]
+                   if target_index & 0x88:
+                       continue
+                   target = libchess.Square.from_0x88_index(target_index)
+                   if self.get(target) and self.get(target).get_color() != self.get_turn():
+                       # Promotion.
+                       if target.is_backrank():
+                           for promote_to in "bnrq":
+                               yield libchess.Move.get_promotion_move(self, square, target, promote_to)
+                       else:
+                           yield libchess.Move.get_normal_move(self, square, target)
+                   # En-passant.
+                   elif not self.get(target) and target.get_file() == self._ep_file:
+                       yield libchess.Move.get_en_passant_move(self, square, target)
+            # Other pieces.
+            else:
+                for offset in PIECE_OFFSETS[piece.get_type()]:
+                    target_index = square.get_0x88_index()
+                    while True:
+                        target_index += offset
+                        if target_index & 0x88:
+                            break
+                        target = libchess.Square.from_0x88_index(target_index)
+                        if not self.get(target):
+                            yield libchess.Move.get_normal_move(self, square, target)
+                        else:
+                            if self.get(target).get_color() == self.get_turn():
+                                break
+                            yield libchess.Move.get_normal_move(self, square, target)
+                            break
+
+                        # Knight and king do not go multiple times in their
+                        # direction.
+                        if piece.get_type() in ["n", "k"]:
+                            break
+
+        opponent = libchess.opposite_color(self.get_turn())
+
+        # King-side castling.
+        k = "k" if self.get_turn() == "b" else "K"
+        if self.get_castling_right(k):
+            of = self.get_king(self.get_turn()).get_0x88_index()
+            to = of + 2
+            if not self._board[of + 1] and not self._board[to] and not self.is_check() and not self.is_attacked(opponent, libchess.Square.from_0x88_index(of + 1)) and not self.is_attacked(opponent, libchess.Square.from_0x88_index(to)):
+                yield libchess.Move.get_castling_move(self, libchess.Square.from_0x88_index(of), libchess.Square.from_0x88_index(to), "k")
+
+        # Queen-side castling
+        q = "q" if self.get_turn() == "b" else "Q"
+        if self.get_castling_right(q):
+            of = self.get_king(self.get_turn()).get_0x88_index()
+            to = of - 2
+
+            if not self._board[of - 1] and not self._board[of - 2] and not self._board[of - 3] and not self.is_check() and not self.is_attacked(opponent, libchess.Square.from_0x88_index(of - 1)) and not self.is_attacked(opponent, libchess.Square.from_0x88_index(to)):
+                yield libchess.Move.get_castling_move(self, libchess.Square.from_0x88_index(of), libchess.Square.from_0x88_index(to), "q")
+
+    def get_legal_moves(self):
+        """Gets legal moves in the current position.
+
+        Yields: All legal moves.
+        """
+        for move in self.get_pseudo_legal_moves():
+            potential_position = self.copy()
+            potential_position.make_move(move)
+            if not potential_position.is_king_attacked(self.get_turn()):
+                yield move
+
+    def get_attackers(self, color, square):
+        """Gets the attackers of a specific square.
+
+        Args:
+            color: Filter by this color.
+            square: The square to check for.
+
+        Yields:
+            Source squares of the attack.
+        """
+        assert color in ["b", "w"]
+
+        ATTACKS = [
+            20, 0, 0, 0, 0, 0, 0, 24, 0, 0, 0, 0, 0, 0, 20, 0,
+            0, 20, 0, 0, 0, 0, 0, 24, 0, 0, 0, 0, 0, 20, 0, 0,
+            0, 0, 20, 0, 0, 0, 0, 24, 0, 0, 0, 0, 20, 0, 0, 0,
+            0, 0, 0, 20, 0, 0, 0, 24, 0, 0, 0, 20, 0, 0, 0, 0,
+            0, 0, 0, 0, 20, 0, 0, 24, 0, 0, 20, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 20, 2, 24, 2, 20, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 2, 53, 56, 53, 2, 0, 0, 0, 0, 0, 0,
+            24, 24, 24, 24, 24, 24, 56, 0, 56, 24, 24, 24, 24, 24, 24, 0,
+            0, 0, 0, 0, 0, 2, 53, 56, 53, 2, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 20, 2, 24, 2, 20, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 20, 0, 0, 24, 0, 0, 20, 0, 0, 0, 0, 0,
+            0, 0, 0, 20, 0, 0, 0, 24, 0, 0, 0, 20, 0, 0, 0, 0,
+            0, 0, 20, 0, 0, 0, 0, 24, 0, 0, 0, 0, 20, 0, 0, 0,
+            0, 20, 0, 0, 0, 0, 0, 24, 0, 0, 0, 0, 0, 20, 0, 0,
+            20, 0, 0, 0, 0, 0, 0, 24, 0, 0, 0, 0, 0, 0, 20
+        ]
+
+        RAYS = [
+            17, 0, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 15, 0,
+            0, 17, 0, 0, 0, 0, 0, 16, 0, 0, 0, 0, 0, 15, 0, 0,
+            0, 0, 17, 0, 0, 0, 0, 16, 0, 0, 0, 0, 15, 0, 0, 0,
+            0, 0, 0, 17, 0, 0, 0, 16, 0, 0, 0, 15, 0, 0, 0, 0,
+            0, 0, 0, 0, 17, 0, 0, 16, 0, 0, 15, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 17, 0, 16, 0, 15, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 17, 16, 15, 0, 0, 0, 0, 0, 0, 0,
+            1, 1, 1, 1, 1, 1, 1, 0, -1, -1, -1, -1, -1, -1, -1, 0,
+            0, 0, 0, 0, 0, 0, -15, -16, -17, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, -15, 0, -16, 0, -17, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, -15, 0, 0, -16, 0, 0, -17, 0, 0, 0, 0, 0,
+            0, 0, 0, -15, 0, 0, 0, -16, 0, 0, 0, -17, 0, 0, 0, 0,
+            0, 0, -15, 0, 0, 0, 0, -16, 0, 0, 0, 0, -17, 0, 0, 0,
+            0, -15, 0, 0, 0, 0, 0, -16, 0, 0, 0, 0, 0, -17, 0, 0,
+            -15, 0, 0, 0, 0, 0, 0, -16, 0, 0, 0, 0, 0, 0, -17
+        ]
+
+        SHIFTS = {
+            "p": 0,
+            "n": 1,
+            "b": 2,
+            "r": 3,
+            "q": 4,
+            "k": 5
+        }
+
+        for source in libchess.Square.get_all():
+            piece = self.get(source)
+            if not piece or piece.get_color() != color:
+                continue
+
+            difference = source.get_0x88_index() - square.get_0x88_index()
+            index = difference + 119
+
+            if ATTACKS[index] & (1 << libchess.SHIFTS[piece.get_type()]):
+                # Handle pawns.
+                if piece.get_type() == "p":
+                    if difference > 0:
+                        if piece.get_color() == "w":
+                            yield source
+                    else:
+                        if piece.get_color() == "b":
+                            yield source
+                    continue
+
+                # Handle knights and king.
+                if piece.get_type() in ["n", "k"]:
+                    yield source
+
+                # Handle the others.
+                offset = RAYS[index]
+                j = source.get_0x88_index() + offset
+                blocked = False
+                while j != square.get_0x88_index():
+                    if self._board[j]:
+                        blocked = True
+                        break
+                    j += offset
+                if not blocked:
+                    yield source
+
+    def is_attacked(self, color, square):
+        """Checks whether a square is attacked.
+
+        Args:
+            color: Check if this player is attacking.
+            square: The square he might be attacking.
+
+        Returns:
+            A boolean indicating whether the given square is attacked by the
+            player of the given color.
+        """
+        try:
+            self.get_attackers(color, square).next()
+            return True
+        except StopIteration:
+            return False
+
+    def is_check(self):
+        """Checks for checks.
+
+        Returns:
+            A boolean indicating whether the current player is in check.
+        """
+        return self.is_king_attacked(self.get_turn())
+
+    def is_checkmate(self):
+        """Checks for checkmates.
+
+        Returns:
+            A boolean indicating whether the current player has been
+            checkmated.
+        """
+        if not self.is_check():
+            return False
+        else:
+            try:
+                self.get_legal_moves().next()
+                return False
+            except StopIteration:
+                return True
+
+    def is_stalemate(self):
+        """Checks for stalemates.
+
+        Returns:
+            A boolean indicating whether the current player is in stalemate.
+        """
+        if self.is_check():
+            return False
+        else:
+            try:
+                self.get_legal_moves().next()
+                return False
+            except StopIteration:
+                return True
+
+    def is_insufficient_material(self):
+        """Checks if thee is sufficient material to mate.
+
+        Mating is impossible in:
+        - A king versus king endgame.
+        - A king with bishop versus king endgame.
+        - A king with knight versus king endgame.
+        - A king with bishop versus king with bishop endgame, where both
+          bishops are on the same color. Same goes for additional bishops on
+          the same color.
+
+        Assumes that the position is valid and each player has exactly one
+        king.
+
+        Returns:
+            A boolean indicating whether there is insufficient material to
+            mate.
+        """
+        piece_counts = self.get_piece_counts()
+        if sum(piece_counts.values()) == 2:
+            # King versus king.
+            return True
+        elif sum(piece_counts.values()) == 3:
+            # King and knight or bishop versus king.
+            if piece_counts["b"] == 1 or piece_counts["n"] == 1:
+                return True
+        elif sum(piece_counts.values()) == 2 + piece_counts["b"]:
+            # Each player with only king and any number of bishops, where all
+            # bishops are on the same color.
+            white_has_bishop = self.get_piece_counts("w")["b"] != 0
+            black_has_bishop = self.get_piece_counts("b")["b"] != 0
+            if white_has_bishop and black_has_bishop:
+                color = None
+                for square in libchess.Square.get_all():
+                    if self.get(square) and self.get(square).get_type() == "b":
+                        if color != None and color != square.is_light():
+                            return False
+                        color = square.is_light()
+                return True
+        return False
+
+    def is_game_over(self):
+        """Checks if the game is over by the rules of chess, disregarding that
+        players can agree on a draw, claim a draw or resign.
+
+        Returns:
+            A boolean indicating whether the game is over.
+        """
+        return self.is_checkmate() or self.is_stalemate() or self.is_insufficient_material()
 
     @staticmethod
     def get_default():
