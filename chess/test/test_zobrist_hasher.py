68e373bdf575d6522c957f32d8eccea18210b454
Niklas Fiekas <niklas.fiekas@googlemail.com>
2012-10-06 19:28:45 +0200

    Fix hashing in the en-passant part.

--- chess/test/test_zobrist_hasher.py
+++ chess/test/test_zobrist_hasher.py
@@ -5,13 +5,23 @@ class ZobristHasherTestCase(unittest.TestCase):
     """Tests the ZobristHasher class."""
 
     def test_polyglot_hashing(self):
-        hasher = chess.ZobristHasher()
+        hasher = chess.ZobristHasher(chess.ZobristHasher.POLYGLOT_RANDOM_ARRAY)
 
         pos = chess.Position()
         self.assertEqual(hasher.hash_position(pos), 0x463b96181691fc9c)
+
+        pos = chess.Position("rnbqkbnr/p1pppppp/8/8/P6P/R1p5/1P1PPPP1/1NBQKBNR b Kkq - 0 4")
+        self.assertEqual(hasher.hash_position(pos), 0x5c3f9b829b279560)
 
         pos = chess.Position("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1")
         self.assertEqual(hasher.hash_position(pos), 0x823c9b50fd114196)
 
         pos = chess.Position("rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 2")
         self.assertEqual(hasher.hash_position(pos), 0x0756b94461c50fb0)
+
+        pos = chess.Position("rnbq1bnr/ppp1pkpp/8/3pPp2/8/8/PPPPKPPP/RNBQ1BNR w - - 0 4")
+        self.assertEqual(hasher.hash_position(pos), 0x00fdd303c946bdd9)
+
+        # Real en-passant possible.
+        pos = chess.Position("rnbqkbnr/ppp1p1pp/8/3pPp2/8/8/PPPP1PPP/RNBQKBNR w KQkq f6 0 3")
+        self.assertEqual(hasher.hash_position(pos), 0x22a48b5a8e47ff78)
--- chess/zobrist_hasher.py
+++ chess/zobrist_hasher.py
@@ -240,7 +240,17 @@ class ZobristHasher(object):
 
         # Hash in the en-passant file.
         if position.get_ep_file():
-            key ^= self._random_array[772 + ord(position.get_ep_file()) - ord("a")]
+            f = ord(position.get_ep_file()) - ord("a")
+            if position.get_turn() == "b":
+                if f > 0 and position.get(chess.Square.from_x_and_y(f - 1, 3)) and position.get(chess.Square.from_x_and_y(f - 1, 3)).get_symbol() == "p":
+                    key ^= self._random_array[772 + f]
+                elif f < 7 and position.get(chess.Square.from_x_and_y(f + 1, 3)) and position.get(chess.Square.from_x_and_y(f + 1, 3)).get_symbol() == "p":
+                    key ^= self._random_array[772 + f]
+            else:
+                if f > 0 and position.get(chess.Square.from_x_and_y(f - 1, 4)) and position.get(chess.Square.from_x_and_y(f - 1, 4)).get_symbol() == "P":
+                    key ^= self._random_array[772 + f]
+                elif f < 7 and position.get(chess.Square.from_x_and_y(f + 1, 4)) and position.get(chess.Square.from_x_and_y(f + 1, 4)).get_symbol() == "P":
+                    key ^= self._random_array[772 + f]
 
         # Hash in the turn.
         if position.get_turn() == "w":
