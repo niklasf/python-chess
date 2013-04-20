211fc0caf56ebe3edc303898d45f2ebf92098a0f
Niklas Fiekas <niklas.fiekas@googlemail.com>
2013-04-20 23:13:04 +0200

    Add missing header file.

---
+++ libchess/move.h
@@ -0,0 +1,43 @@
+#ifndef LIBCHESS_MOVE_H
+#define LIBCHESS_MOVE_H
+
+#include "square.h"
+
+#include <iostream>
+
+namespace chess {
+
+    class Move {
+
+    public:
+        Move(Square source, Square target, char promotion);
+        Move(Square source, Square target);
+        Move(std::string uci);
+
+        Square source() const;
+        Square target() const;
+        char promotion() const;
+        std::string full_promotion() const;
+        std::string uci() const;
+
+        bool is_promotion() const;
+
+        std::string __repr__() const;
+        int __hash__() const;
+
+        bool operator==(const Move& other) const;
+        bool operator!=(const Move& other) const;
+
+        static Move from_uci(std::string uci);
+
+    private:
+        Square m_source;
+        Square m_target;
+        char m_promotion;
+    };
+
+    std::ostream& operator<<(std::ostream &out, const Move& move);
+
+}
+
+#endif
