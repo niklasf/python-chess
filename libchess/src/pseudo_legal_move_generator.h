67d184abbd89ba31f2a19e205f0cb2a8fa971efc
Niklas Fiekas <niklas.fiekas@googlemail.com>
2013-05-10 15:07:19 +0200

    Reindent PseudoLegalMoveGenerator.

--- libchess/src/pseudo_legal_move_generator.h
+++ libchess/src/pseudo_legal_move_generator.h
@@ -9,34 +9,34 @@
 
 namespace chess {
 
-    class Position;
+class Position;
 
-    /**
-     * \brief Enumerates pseudo legal moves in a given position.
-     */
-    class PseudoLegalMoveGenerator : boost::noncopyable {
-    public:
-         PseudoLegalMoveGenerator(const Position& position);
-         ~PseudoLegalMoveGenerator();
+/**
+ * \brief Enumerates pseudo legal moves in a given position.
+ */
+class PseudoLegalMoveGenerator : boost::noncopyable {
+public:
+     PseudoLegalMoveGenerator(const Position& position);
+     ~PseudoLegalMoveGenerator();
 
-         int __len__();
-         bool __nonzero__();
-         PseudoLegalMoveGenerator& __iter__();
-         bool __contains__(const Move& move);
+     int __len__();
+     bool __nonzero__();
+     PseudoLegalMoveGenerator& __iter__();
+     bool __contains__(const Move& move);
 
-         bool has_more();
-         Move next();
-         Move python_next();
+     bool has_more();
+     Move next();
+     Move python_next();
 
-    protected:
-         void generate_from_square(const Square& square);
-         std::queue<Move> m_cache;
+protected:
+     void generate_from_square(const Square& square);
+     std::queue<Move> m_cache;
 
-    private:
-         Position* m_position;
-         int m_index;
-    };
+private:
+     Position* m_position;
+     int m_index;
+};
 
-}
+} // namespace chess
 
-#endif
+#endif // LIBCHESS_PSEUDO_LEGAL_MOVE_GENERATOR_H
