44e45602005b2cd378739eca44511c92be538f16
Niklas Fiekas <niklas.fiekas@googlemail.com>
2013-05-10 14:39:50 +0200

    Reindent AttackerGenerator.

--- libchess/src/attacker_generator.h
+++ libchess/src/attacker_generator.h
@@ -5,32 +5,32 @@
 
 namespace chess {
 
-   class Position;
+class Position;
 
-   /**
-    * \brief Enumerates attackers of a given side on a specific square of the
-    *   given position.
-    */
-   class AttackerGenerator : boost::noncopyable {
-   public:
-       AttackerGenerator(const Position& position, char color, Square target);
+/**
+ * \brief Enumerates attackers of a given side on a specific square of the
+ *   given position.
+ */
+class AttackerGenerator : boost::noncopyable {
+public:
+    AttackerGenerator(const Position& position, char color, Square target);
 
-       int __len__();
-       bool __nonzero__();
-       AttackerGenerator& __iter__();
-       bool __contains__(Square square);
+    int __len__();
+    bool __nonzero__();
+    AttackerGenerator& __iter__();
+    bool __contains__(const Square& square);
 
-       bool has_more();
-       Square next();
-       Square python_next();
+    bool has_more();
+    Square next();
+    Square python_next();
 
-   private:
-       char m_color;
-       const Position& m_position;
-       Square m_target;
-       int m_source_index;
-   };
+private:
+    char m_color;
+    const Position& m_position;
+    Square m_target;
+    int m_source_index;
+};
 
-}
+} // namespace chess
 
-#endif
+#endif // LIBCHESS_ATTACKER_GENERATOR_H
