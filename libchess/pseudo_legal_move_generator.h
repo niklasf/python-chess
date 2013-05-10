82e044787489193e8a07507cf21bfffbbb822a4a
Niklas Fiekas <niklas.fiekas@googlemail.com>
2013-05-11 00:30:32 +0200

    Add license headers to libchess files.

--- libchess/pseudo_legal_move_generator.h
+++ libchess/pseudo_legal_move_generator.h
@@ -1,3 +1,19 @@
+// This file is part of the python-chess library.
+// Copyright (C) 2013 Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have recieved a copy of the GNU General Public License
+// along with this program. If not, see <http://gnu.org/licenses/>.
+
 #ifndef LIBCHESS_PSEUDO_LEGAL_MOVE_GENERATOR_H
 #define LIBCHESS_PSEUDO_LEGAL_MOVE_GENERATOR_H
 
