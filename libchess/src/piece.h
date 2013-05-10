8c80b02ea4b91de6558506fbf980bc6865ed2385
Niklas Fiekas <niklas.fiekas@googlemail.com>
2013-05-10 11:43:57 +0200

    Clean up Piece class.

--- libchess/src/piece.h
+++ libchess/src/piece.h
@@ -5,36 +5,38 @@
 
 namespace chess {
 
-    /**
-     * \brief An immutable chess piece.
-     */
-    class Piece {
+/**
+ * \brief An immutable chess piece.
+ */
+class Piece {
+public:
+    Piece();
+    Piece(char symbol);
+    Piece(const Piece& piece);
 
-    public:
-        Piece();
-        Piece(char symbol);
+    char color() const;
+    char type() const;
+    char symbol() const;
+    std::string full_color() const;
+    std::string full_type() const;
 
-        char color() const;
-        char type() const;
-        char symbol() const;
-        std::string full_color() const;
-        std::string full_type() const;
+    std::string __repr__() const;
+    int __hash__() const;
 
-        std::string __repr__() const;
-        int __hash__() const;
+    bool is_valid() const;
 
-        bool is_valid() const;
+    Piece& operator=(const Piece& rhs);
+    bool operator==(const Piece& rhs) const;
+    bool operator!=(const Piece& rhs) const;
 
-        bool operator==(const Piece& other) const;
-        bool operator!=(const Piece& other) const;
+    static Piece from_color_and_type(char color, char type);
 
-        static Piece from_color_and_type(char color, char type);
+private:
+    char m_symbol;
+};
 
-    private:
-        char m_symbol;
-    };
+std::ostream& operator<<(std::ostream& out, const Piece& piece);
 
-    std::ostream& operator<<(std::ostream& out, const Piece& piece);
-}
+} // namespace chess
 
-#endif
+#endif // LIBCHESS_PIECE_H
