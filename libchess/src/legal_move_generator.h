c15ecf463e206729170f0ca9821e057550c13bef
Niklas Fiekas <niklas.fiekas@googlemail.com>
2013-05-10 15:08:15 +0200

    Reindent LegalMoveGenerator.

--- libchess/src/legal_move_generator.h
+++ libchess/src/legal_move_generator.h
@@ -8,33 +8,33 @@
 
 namespace chess {
 
-    class Position;
+class Position;
 
-    /**
-     * \brief Enumerates legal moves in a given position.
-     */
-    class LegalMoveGenerator {
-    public:
-        LegalMoveGenerator(const Position& position);
+/**
+ * \brief Enumerates legal moves in a given position.
+ */
+class LegalMoveGenerator {
+public:
+    LegalMoveGenerator(const Position& position);
 
-        int __len__();
-        bool __nonzero__();
-        LegalMoveGenerator __iter__();
-        bool __contains__(Move move);
+    int __len__();
+    bool __nonzero__();
+    LegalMoveGenerator __iter__();
+    bool __contains__(Move move);
 
-        Move next();
-        Move python_next();
-        bool has_more();
+    Move next();
+    Move python_next();
+    bool has_more();
 
-    private:
-        bool would_be_valid_if_pseudo_legal(const Move& move) const;
+private:
+    bool would_be_valid_if_pseudo_legal(const Move& move) const;
 
-        const boost::shared_ptr<Position> m_position;
-        const boost::shared_ptr<PseudoLegalMoveGenerator> m_pseudo_legal_moves;
-        int m_len;
-        int m_current;
-    };
+    const boost::shared_ptr<Position> m_position;
+    const boost::shared_ptr<PseudoLegalMoveGenerator> m_pseudo_legal_moves;
+    int m_len;
+    int m_current;
+};
 
-}
+} // namespace chess
 
-#endif
+#endif // LIBCHESS_LEGAL_MOVE_GENERATOR_H
