4b70c7fe1acbb32be0ffdd5ecfad6650d291f7b5
Niklas Fiekas <niklas.fiekas@googlemail.com>
2013-05-10 12:03:28 +0200

    Clean up Move indentation.

--- libchess/src/move.cc
+++ libchess/src/move.cc
@@ -4,126 +4,126 @@
 
 namespace chess {
 
-    Move::Move(Square source, Square target, char promotion) : m_source(source), m_target(target) {
-        if (!source.is_valid()) {
-            throw new std::invalid_argument("source");
-        }
-
-        if (!target.is_valid()) {
-            throw new std::invalid_argument("target");
-        }
-
-        switch (promotion) {
-            case 'r':
-            case 'n':
-            case 'b':
-            case 'q':
-                m_promotion = promotion;
-                break;
-            default:
-                throw new std::invalid_argument("promotion");
-        }
+Move::Move(Square source, Square target, char promotion) : m_source(source), m_target(target) {
+    if (!source.is_valid()) {
+        throw std::invalid_argument("source");
     }
 
-    Move::Move(Square source, Square target) : m_source(source), m_target(target) {
-        if (!source.is_valid()) {
-            throw new std::invalid_argument("source");
-        }
-
-        if (!target.is_valid()) {
-            throw new std::invalid_argument("target");
-        }
-
-
-        m_promotion = 0;
+    if (!target.is_valid()) {
+        throw std::invalid_argument("target");
     }
 
-    Move::Move(std::string uci) : m_source(Square(0)), m_target(Square(0)) {
-        if (uci.length() == 4 || uci.length() == 5) {
-            m_source = Square(uci.substr(0, 2));
-            m_target = Square(uci.substr(2, 2));
-            if (uci.length() == 5) {
-                m_promotion = uci.at(4);
-                switch (m_promotion) {
-                    case 'r':
-                    case 'n':
-                    case 'b':
-                    case 'q':
-                        break;
-                    default:
-                        throw new std::invalid_argument("uci");
-                }
-            } else {
-                m_promotion = 0;
-            }
-        }
-        else {
-            throw new std::invalid_argument("uci");
-        }
+    switch (promotion) {
+        case 'r':
+        case 'n':
+        case 'b':
+        case 'q':
+            m_promotion = promotion;
+            break;
+        default:
+            throw std::invalid_argument("promotion");
     }
-
-    Square Move::source() const {
-        return m_source;
-    }
-
-    Square Move::target() const {
-        return m_target;
-    }
-
-    char Move::promotion() const {
-        return m_promotion;
-    }
-
-    std::string Move::full_promotion() const {
-        switch (m_promotion) {
-            case 'r':
-                return "rook";
-            case 'b':
-                return "bishop";
-            case 'n':
-                return "knight";
-            case 'q':
-                return "queen";
-        }
-
-        throw std::logic_error("Unknown promotion type.");
-    }
-
-    std::string Move::uci() const {
-        if (m_promotion) {
-            return m_source.name() + m_target.name() + m_promotion;
-        } else {
-            return m_source.name() + m_target.name();
-        }
-    }
-
-    bool Move::is_promotion() const {
-        return m_promotion != 0;
-    }
-
-    std::string Move::__repr__() const {
-        return boost::str(boost::format("Move.from_uci('%s')") % uci());
-    }
-
-    int Move::__hash__() const {
-        return m_source.__hash__() + 100 * m_target.__hash__() + 10000 * m_promotion;
-    }
-
-    bool Move::operator==(const Move& other) const {
-        return m_source == other.m_source && m_target == other.m_target && m_promotion == other.m_promotion;
-    }
-
-    bool Move::operator!=(const Move& other) const {
-        return m_source != other.m_source || m_target != other.m_target || m_promotion != other.m_promotion;
-    }
-
-    Move Move::from_uci(std::string uci) {
-        return Move(uci);
-    }
-
-    std::ostream& operator<<(std::ostream& out, const Move& move) {
-        out << move.uci();
-        return out;
-    }
-
 }
+
+Move::Move(Square source, Square target) : m_source(source), m_target(target) {
+    if (!source.is_valid()) {
+        throw std::invalid_argument("source");
+    }
+
+    if (!target.is_valid()) {
+        throw std::invalid_argument("target");
+    }
+
+
+    m_promotion = 0;
+}
+
+Move::Move(std::string uci) : m_source(Square(0)), m_target(Square(0)) {
+    if (uci.length() == 4 || uci.length() == 5) {
+        m_source = Square(uci.substr(0, 2));
+        m_target = Square(uci.substr(2, 2));
+        if (uci.length() == 5) {
+            m_promotion = uci.at(4);
+            switch (m_promotion) {
+                case 'r':
+                case 'n':
+                case 'b':
+                case 'q':
+                    break;
+                default:
+                    throw std::invalid_argument("uci");
+            }
+        } else {
+            m_promotion = 0;
+        }
+    }
+    else {
+        throw new std::invalid_argument("uci");
+    }
+}
+
+Square Move::source() const {
+    return m_source;
+}
+
+Square Move::target() const {
+    return m_target;
+}
+
+char Move::promotion() const {
+    return m_promotion;
+}
+
+std::string Move::full_promotion() const {
+    switch (m_promotion) {
+        case 'r':
+            return "rook";
+        case 'b':
+            return "bishop";
+        case 'n':
+            return "knight";
+        case 'q':
+            return "queen";
+    }
+
+    throw std::logic_error("Unknown promotion type.");
+}
+
+std::string Move::uci() const {
+    if (m_promotion) {
+        return m_source.name() + m_target.name() + m_promotion;
+    } else {
+        return m_source.name() + m_target.name();
+    }
+}
+
+bool Move::is_promotion() const {
+    return m_promotion != 0;
+}
+
+std::string Move::__repr__() const {
+    return boost::str(boost::format("Move.from_uci('%s')") % uci());
+}
+
+int Move::__hash__() const {
+    return m_source.__hash__() + 100 * m_target.__hash__() + 10000 * m_promotion;
+}
+
+bool Move::operator==(const Move& other) const {
+    return m_source == other.m_source && m_target == other.m_target && m_promotion == other.m_promotion;
+}
+
+bool Move::operator!=(const Move& other) const {
+    return m_source != other.m_source || m_target != other.m_target || m_promotion != other.m_promotion;
+}
+
+Move Move::from_uci(std::string uci) {
+    return Move(uci);
+}
+
+std::ostream& operator<<(std::ostream& out, const Move& move) {
+    out << move.uci();
+    return out;
+}
+
+} // namespace chess
--- libchess/src/move.h
+++ libchess/src/move.h
@@ -7,40 +7,39 @@
 
 namespace chess {
 
-    /**
-     * \brief An immutable move.
-     */
-    class Move {
+/**
+ * \brief An immutable move.
+ */
+class Move {
+public:
+    Move(Square source, Square target, char promotion);
+    Move(Square source, Square target);
+    Move(std::string uci);
 
-    public:
-        Move(Square source, Square target, char promotion);
-        Move(Square source, Square target);
-        Move(std::string uci);
+    Square source() const;
+    Square target() const;
+    char promotion() const;
+    std::string full_promotion() const;
+    std::string uci() const;
 
-        Square source() const;
-        Square target() const;
-        char promotion() const;
-        std::string full_promotion() const;
-        std::string uci() const;
+    bool is_promotion() const;
 
-        bool is_promotion() const;
+    std::string __repr__() const;
+    int __hash__() const;
 
-        std::string __repr__() const;
-        int __hash__() const;
+    bool operator==(const Move& other) const;
+    bool operator!=(const Move& other) const;
 
-        bool operator==(const Move& other) const;
-        bool operator!=(const Move& other) const;
+    static Move from_uci(std::string uci);
 
-        static Move from_uci(std::string uci);
+private:
+    Square m_source;
+    Square m_target;
+    char m_promotion;
+};
 
-    private:
-        Square m_source;
-        Square m_target;
-        char m_promotion;
-    };
+std::ostream& operator<<(std::ostream &out, const Move& move);
 
-    std::ostream& operator<<(std::ostream &out, const Move& move);
+} // namespace chess
 
-}
-
-#endif
+#endif // LIBCHESS_MOVE_H
