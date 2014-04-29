9b30f88125d900f98ca094a9500fce957e8ed7c4
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2014-04-29 17:13:11 +0200

    Use lists instead of tuples

--- chess.py
+++ chess.py
@@ -1,12 +1,12 @@
 import sys
 
 
-COLORS = ( WHITE, BLACK ) = range(0, 2)
+COLORS = [ WHITE, BLACK ] = range(0, 2)
 
-PIECE_TYPES = ( NONE, PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING ) = range(0, 7)
+PIECE_TYPES = [ NONE, PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING ] = range(0, 7)
 
 
-SQUARES = (
+SQUARES = [
     A1, B1, C1, D1, E1, F1, G1, H1,
     A2, B2, C2, D2, E2, F2, G2, H2,
     A3, B3, C3, D3, E3, F3, G3, H3,
@@ -14,9 +14,9 @@ SQUARES = (
     A5, B5, C5, D5, E5, F5, G5, H5,
     A6, B6, C6, D6, E6, F6, G6, H6,
     A7, B7, C7, D7, E7, F7, G7, H7,
-    A8, B8, C8, D8, E8, F8, G8, H8 ) = range(0, 64)
+    A8, B8, C8, D8, E8, F8, G8, H8 ] = range(0, 64)
 
-SQUARES_L90 = (
+SQUARES_L90 = [
     H1, H2, H3, H4, H5, H6, H7, H8,
     G1, G2, G3, G4, G5, G6, G7, G8,
     F1, F2, F3, F4, F5, F6, F7, F8,
@@ -24,9 +24,9 @@ SQUARES_L90 = (
     D1, D2, D3, D4, D5, D6, D7, D8,
     C1, C2, C3, C4, C5, C6, C7, C8,
     B1, B2, B3, B4, B5, B6, B7, B8,
-    A1, A2, A3, A4, A5, A6, A7, A8 )
+    A1, A2, A3, A4, A5, A6, A7, A8 ]
 
-SQUARES_R45 = (
+SQUARES_R45 = [
     A1, B8, C7, D6, E5, F4, G3, H2,
     A2, B1, C8, D7, E6, F5, G4, H3,
     A3, B2, C1, D8, E7, F6, G5, H4,
@@ -34,9 +34,9 @@ SQUARES_R45 = (
     A5, B4, C3, D2, E1, F8, G7, H6,
     A6, B5, C4, D3, E2, F1, G8, H7,
     A7, B6, C5, D4, E3, F2, G1, H8,
-    A8, B7, C6, D5, E4, F3, G2, H1 )
+    A8, B7, C6, D5, E4, F3, G2, H1 ]
 
-SQUARES_L45 = (
+SQUARES_L45 = [
     A2, B3, C4, D5, E6, F7, G8, H1,
     A3, B4, C5, D6, E7, F8, G1, H2,
     A4, B5, C6, D7, E8, F1, G2, H3,
@@ -44,14 +44,14 @@ SQUARES_L45 = (
     A6, B7, C8, D1, E2, F3, G4, H5,
     A7, B8, C1, D2, E3, F4, G5, H6,
     A8, B1, C2, D3, E4, F5, G6, H7,
-    A1, B2, C3, D4, E5, F6, G7, H8 )
+    A1, B2, C3, D4, E5, F6, G7, H8 ]
 
 
 BB_VOID = 0x0000000000000000L
 
 BB_ALL = 0xffffffffffffffffL
 
-BB_SQUARES = (
+BB_SQUARES = [
     BB_A1, BB_B1, BB_C1, BB_D1, BB_E1, BB_F1, BB_G1, BB_H1,
     BB_A2, BB_B2, BB_C2, BB_D2, BB_E2, BB_F2, BB_G2, BB_H2,
     BB_A3, BB_B3, BB_C3, BB_D3, BB_E3, BB_F3, BB_G3, BB_H3,
@@ -60,9 +60,9 @@ BB_SQUARES = (
     BB_A6, BB_B6, BB_C6, BB_D6, BB_E6, BB_F6, BB_G6, BB_H6,
     BB_A7, BB_B7, BB_C7, BB_D7, BB_E7, BB_F7, BB_G7, BB_H7,
     BB_A8, BB_B8, BB_C8, BB_D8, BB_E8, BB_F8, BB_G8, BB_H8
-) = [ 1L << i for i in SQUARES ]
+] = [ 1L << i for i in SQUARES ]
 
-BB_FILES = (
+BB_FILES = [
     BB_FILE_A,
     BB_FILE_B,
     BB_FILE_C,
@@ -71,7 +71,7 @@ BB_FILES = (
     BB_FILE_F,
     BB_FILE_G,
     BB_FILE_H
-) = (
+] = [
     BB_A1 | BB_A2 | BB_A3 | BB_A4 | BB_A5 | BB_A6 | BB_A7 | BB_A8,
     BB_B1 | BB_B2 | BB_B3 | BB_B4 | BB_B5 | BB_B6 | BB_B7 | BB_B8,
     BB_C1 | BB_C2 | BB_C3 | BB_C4 | BB_C5 | BB_C6 | BB_C7 | BB_C8,
@@ -80,9 +80,9 @@ BB_FILES = (
     BB_F1 | BB_F2 | BB_F3 | BB_F4 | BB_F5 | BB_F6 | BB_F7 | BB_F8,
     BB_G1 | BB_G2 | BB_G3 | BB_G4 | BB_G5 | BB_G6 | BB_G7 | BB_G8,
     BB_H1 | BB_H2 | BB_H3 | BB_H4 | BB_H5 | BB_H6 | BB_H7 | BB_H8
-)
+]
 
-BB_RANKS = (
+BB_RANKS = [
     BB_RANK_1,
     BB_RANK_2,
     BB_RANK_3,
@@ -91,7 +91,7 @@ BB_RANKS = (
     BB_RANK_6,
     BB_RANK_7,
     BB_RANK_8
-) = (
+] = [
     BB_A1 | BB_B1 | BB_C1 | BB_D1 | BB_E1 | BB_F1 | BB_G1 | BB_H1,
     BB_A2 | BB_B2 | BB_C2 | BB_D2 | BB_E2 | BB_F2 | BB_G2 | BB_H2,
     BB_A3 | BB_B3 | BB_C3 | BB_D3 | BB_E3 | BB_F3 | BB_G3 | BB_H3,
@@ -100,7 +100,7 @@ BB_RANKS = (
     BB_A6 | BB_B6 | BB_C6 | BB_D6 | BB_E6 | BB_F6 | BB_G6 | BB_H6,
     BB_A7 | BB_B7 | BB_C7 | BB_D7 | BB_E7 | BB_F7 | BB_G7 | BB_H7,
     BB_A8 | BB_B8 | BB_C8 | BB_D8 | BB_E8 | BB_F8 | BB_G8 | BB_H8
-)
+]
 
 
 def shift_down(b):
@@ -140,28 +140,34 @@ def shift_down_right(b):
     return (b >> 7) & ~BB_FILE_A
 
 
-BB_KNIGHT_ATTACKS = tuple((
-       shift_left(shift_2_up(square))
-     | shift_right(shift_2_up(square))
-     | shift_left(shift_2_down(square))
-     | shift_right(shift_2_down(square))
-     | shift_2_left(shift_up(square))
-     | shift_2_right(shift_up(square))
-     | shift_2_left(shift_down(square))
-     | shift_2_right(shift_down(square))
-    for square in BB_SQUARES))
+BB_KNIGHT_ATTACKS = []
+
+for square in BB_SQUARES:
+    mask = BB_VOID
+    mask |= shift_left(shift_2_up(square))
+    mask |= shift_right(shift_2_up(square))
+    mask |= shift_left(shift_2_down(square))
+    mask |= shift_right(shift_2_down(square))
+    mask |= shift_2_left(shift_up(square))
+    mask |= shift_2_right(shift_up(square))
+    mask |= shift_2_left(shift_down(square))
+    mask |= shift_2_right(shift_down(square))
+    BB_KNIGHT_ATTACKS.append(square)
 
 
-BB_KING_ATTACKS = tuple((
-       shift_left(square)
-     | shift_right(square)
-     | shift_up(square)
-     | shift_down(square)
-     | shift_up_left(square)
-     | shift_up_right(square)
-     | shift_down_left(square)
-     | shift_down_right(square)
-    for square in BB_SQUARES))
+BB_KING_ATTACKS = []
+
+for square in BB_SQUARES:
+    mask = BB_VOID
+    mask |= shift_left(square)
+    mask |= shift_right(square)
+    mask |= shift_up(square)
+    mask |= shift_down(square)
+    mask |= shift_up_left(square)
+    mask |= shift_up_right(square)
+    mask |= shift_down_left(square)
+    mask |= shift_down_right(square)
+    BB_KING_ATTACKS.append(mask)
 
 
 
