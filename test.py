a2fa4655d49f626fed9817de87bbce1f52460c08
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2014-05-03 16:16:15 +0200

    Add failing test for FEN

--- test.py
+++ test.py
@@ -16,5 +16,16 @@ class BitboardTestCase(unittest.TestCase):
         bitboard = chess.Bitboard()
         bitboard.push(chess.Move(chess.E2, chess.E4))
 
+    def test_fen(self):
+        bitboard = chess.Bitboard()
+        self.assertEqual(bitboard.fen(), chess.STARTING_FEN)
+
+        fen = "6k1/pb3pp1/1p2p2p/1Bn1P3/8/5N2/PP1q1PPP/6K1 w - - 0 24"
+        bitboard.set_fen(fen)
+        self.assertEqual(bitboard.fen(), fen)
+
+        bitboard.push(chess.Move.from_uci("f3d2"))
+        self.assertEqual(bitboard.fen(), "6k1/pb3pp1/1p2p2p/1Bn1P3/8/8/PP1N1PPP/6K1 b - - 0 24")
+
 if __name__ == "__main__":
     unittest.main()
