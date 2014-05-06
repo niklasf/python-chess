b91994e1222d1d4ec663f30bc824d9ff02f5b3c5
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2014-05-06 19:43:32 +0200

    Add polyglot compatible zobrist hashing

--- test.py
+++ test.py
@@ -273,6 +273,49 @@ class BitboardTestCase(unittest.TestCase):
 
         self.assertEqual(bitboard.fen(), fen)
 
+    def test_polyglot(self):
+        board = chess.Bitboard()
+        self.assertEqual(board.fen(), "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        self.assertEqual(board.__hash__(), 0x463b96181691fc9c)
+
+        board.push_san("e4")
+        self.assertEqual(board.fen(), "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1")
+        #self.assertEqual(board.__hash__(), 0x823c9b50fd114196)
+
+        board.push_san("d5")
+        self.assertEqual(board.fen(), "rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 2")
+        #self.assertEqual(board.__hash__(), 0x0756b94461c50fb0)
+
+        board.push_san("e5")
+        self.assertEqual(board.fen(), "rnbqkbnr/ppp1pppp/8/3pP3/8/8/PPPP1PPP/RNBQKBNR b KQkq - 0 2")
+        self.assertEqual(board.__hash__(), 0x662fafb965db29d4)
+
+        board.push_san("f5")
+        self.assertEqual(board.fen(), "rnbqkbnr/ppp1p1pp/8/3pPp2/8/8/PPPP1PPP/RNBQKBNR w KQkq f6 0 3")
+        self.assertEqual(board.__hash__(), 0x22a48b5a8e47ff78)
+
+        board.push_san("Ke2")
+        #self.assertEqual(board.fen(), "rnbqkbnr/ppp1p1pp/8/3pPp2/8/8/PPPPKPPP/RNBQ1BNR b kq - 0 3")
+        self.assertEqual(board.__hash__(), 0x652a607ca3f242c1)
+
+        board.push_san("Kf7")
+        #self.assertEqual(board.fen(), "rnbq1bnr/ppp1pkpp/8/3pPp2/8/8/PPPPKPPP/RNBQ1BNR w - - 0 4")
+        self.assertEqual(board.__hash__(), 0x00fdd303c946bdd9)
+
+        board = chess.Bitboard()
+        board.push_san("a4")
+        board.push_san("b5")
+        board.push_san("h4")
+        board.push_san("b4")
+        board.push_san("c4")
+        self.assertEqual(board.fen(), "rnbqkbnr/p1pppppp/8/8/PpP4P/8/1P1PPPP1/RNBQKBNR b KQkq c3 0 3")
+        self.assertEqual(board.__hash__(), 0x3c8123ea7b067637)
+
+        board.push_san("bxc3")
+        board.push_san("Ra3")
+        #self.assertEqual(board.fen(), "rnbqkbnr/p1pppppp/8/8/P6P/R1p5/1P1PPPP1/1NBQKBNR b Kkq - 0 4")
+        self.assertEqual(board.__hash__(), 0x5c3f9b829b279560)
+
 
 class LegalMoveGeneratorTestCase(unittest.TestCase):
 
