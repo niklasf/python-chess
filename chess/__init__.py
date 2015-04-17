4f009279c02b9d3de990c3313c80050d93581c87
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2015-04-18 01:24:25 +0200

    Increment copyright year

--- chess/__init__.py
+++ chess/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of the python-chess library.
-# Copyright (C) 2012-2014 Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
+# Copyright (C) 2012-2015 Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
