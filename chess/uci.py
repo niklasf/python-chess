554d15076ebcb38de04c11b667fc5f82b471d19f
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2015-05-20 11:52:25 +0200

    Remove __future__ imports from uci module

--- chess/uci.py
+++ chess/uci.py
@@ -16,9 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import print_function
-from __future__ import unicode_literals
-
 import chess
 import collections
 import signal
