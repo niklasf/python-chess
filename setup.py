b55af43fd55b156e642e44071d2de31233f5aa80
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2015-10-13 22:36:25 +0200

    Add missing Python 2 trove classifier

--- setup.py
+++ setup.py
@@ -80,6 +80,7 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
