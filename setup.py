418856438b4f71ad7bdd9ee26059005c98f7969f
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2015-10-13 22:34:48 +0200

    Explicitly support 3.5

--- setup.py
+++ setup.py
@@ -85,6 +85,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Topic :: Games/Entertainment :: Board Games",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
