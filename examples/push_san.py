9e808576a30c4ae4d3777f44fb725a0255f350d5
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2015-11-24 13:42:13 +0100

    Add brakto kopec test to tox.ini

--- examples/push_san.py
+++ examples/push_san.py
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import print_function
+
 import chess
 import timeit
 
