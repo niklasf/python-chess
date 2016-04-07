d4d60392b535cd517ab4caccf82dbbde4599476a
Niklas Fiekas <niklas.fiekas@backscattering.de>
2016-04-07 12:38:50 +0200

    Perft test some more tricky positions

--- perft.py
+++ perft.py
@@ -106,6 +106,50 @@ class PerftTestCase(unittest.TestCase):
     def test_speed(self):
         self.execute_test(perft, 10000)
 
+    def test_tricky(self):
+        # Source: https://chessprogramming.wikispaces.com/Perft+Results
+
+        # Position 2: Kiwipete by Peter McKenzie.
+        board = chess.Board("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1")
+        self.assertEqual(perft(board, 1), 48)
+        self.assertEqual(perft(board, 2), 2039)
+        self.assertEqual(perft(board, 3), 97862)
+        sys.stdout.write(".")
+        sys.stdout.flush()
+
+        # Position 3.
+        board = chess.Board("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1")
+        self.assertEqual(perft(board, 1), 14)
+        self.assertEqual(perft(board, 2), 191)
+        self.assertEqual(perft(board, 3), 2812)
+        self.assertEqual(perft(board, 4), 43238)
+        sys.stdout.write(".")
+        sys.stdout.flush()
+
+        # Position 4 (mirrored).
+        board = chess.Board("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1")
+        self.assertEqual(perft(board, 1), 6)
+        self.assertEqual(perft(board, 2), 264)
+        self.assertEqual(perft(board, 3), 9467)
+        sys.stdout.write(".")
+        sys.stdout.flush()
+
+        # Position 5: http://www.talkchess.com/forum/viewtopic.php?t=42463
+        board = chess.Board("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8")
+        self.assertEqual(perft(board, 1), 44)
+        self.assertEqual(perft(board, 2), 1486)
+        self.assertEqual(perft(board, 3), 62379)
+        sys.stdout.write(".")
+        sys.stdout.flush()
+
+        # Position 6 by Steven Edwards.
+        board = chess.Board("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10")
+        self.assertEqual(perft(board, 1), 46)
+        self.assertEqual(perft(board, 2), 2079)
+        self.assertEqual(perft(board, 3), 89890)
+        sys.stdout.write(".")
+        sys.stdout.flush()
+
     def test_chess960(self):
         # Source: http://www.talkchess.com/forum/viewtopic.php?t=55274
 
