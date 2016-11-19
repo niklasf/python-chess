b117a41e32c85a71de3d8d48cd1d1c94c7c2e9ad
Niklas Fiekas <niklas.fiekas@backscattering.de>
2016-11-20 00:13:12 +0100

    Update my contact mail address

--- docs/conf.py
+++ docs/conf.py
@@ -1,20 +1,4 @@
 # -*- coding: utf-8 -*-
-#
-# This file is part of the python-chess library.
-# Copyright (C) 2012-2015 Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 import os
@@ -35,7 +19,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "python-chess"
-copyright = "2014-2015, Niklas Fiekas"
+copyright = "2014-2016, Niklas Fiekas"
 
 # The version.
 version = chess.__version__
