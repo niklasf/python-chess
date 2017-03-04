12fe3e5d62a31fe976a3999c9589d7df42948715
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-03-04 11:37:57 +0100

    Code golf SQUARES_180

--- chess/__init__.py
+++ chess/__init__.py
@@ -94,15 +94,7 @@ SQUARES = [
     A7, B7, C7, D7, E7, F7, G7, H7,
     A8, B8, C8, D8, E8, F8, G8, H8] = range(64)
 
-SQUARES_180 = [
-    A8, B8, C8, D8, E8, F8, G8, H8,
-    A7, B7, C7, D7, E7, F7, G7, H7,
-    A6, B6, C6, D6, E6, F6, G6, H6,
-    A5, B5, C5, D5, E5, F5, G5, H5,
-    A4, B4, C4, D4, E4, F4, G4, H4,
-    A3, B3, C3, D3, E3, F3, G3, H3,
-    A2, B2, C2, D2, E2, F2, G2, H2,
-    A1, B1, C1, D1, E1, F1, G1, H1]
+SQUARES_180 = [sq ^ 0x38 for sq in SQUARES]
 
 SQUARE_NAMES = [
     "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
