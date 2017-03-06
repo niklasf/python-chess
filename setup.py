83b24393849e7a0aefe826f1eec5b9f196365f34
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-03-06 20:24:36 +0100

    Bump changelog to v0.17.0 and support Python 3.6

--- setup.py
+++ setup.py
@@ -111,6 +111,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Games/Entertainment :: Board Games",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
