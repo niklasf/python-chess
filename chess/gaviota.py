8fa7406b5ad38bcf3e1db17bea63ce542b6e5eed
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-07-26 22:36:56 +0200

    Reorder Gaviota board validation for consistency

--- chess/gaviota.py
+++ chess/gaviota.py
@@ -1978,11 +1978,12 @@ class NativeTablebases(object):
         if board.is_insufficient_material():
             return 0
 
+        if board.castling_rights:
+            raise KeyError("gaviota tables do not contain positions with castling rights: {0}".format(board.fen()))
+
         if chess.popcount(board.occupied) > 5:
             raise KeyError("gaviota tables support up to 5 pieces, not {0}: {1}".format(chess.popcount(board.occupied), board.fen()))
 
-        if board.castling_rights:
-            raise KeyError("gaviota tables do not contain positions with castling rights: {0}".format(board.fen()))
 
         stm = ctypes.c_uint(0 if board.turn == chess.WHITE else 1)
         ep_square = ctypes.c_uint(board.ep_square if board.ep_square else 64)
