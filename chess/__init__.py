567cb0f6b88f189121966ae9660f4ab3f637ee33
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-11-11 21:04:59 +0100

    Use slightly better unicode char for empty square

--- chess/__init__.py
+++ chess/__init__.py
@@ -1029,7 +1029,7 @@ class BaseBoard(object):
                 if piece:
                     builder.append(piece.unicode_symbol(invert_color=invert_color))
                 else:
-                    builder.append(".")
+                    builder.append("·")
 
             if borders:
                 builder.append("|")
