a0972c3fc9df2761dd48ee454c8e969d518548e6
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-05-13 11:54:31 +0200

    Fix DTZ off by 1 for 6 piece suicide threat
    Introduced by accident here: c14eaaf3bd0c5b03ace838ce46c0a897157f9aa2
    
    Original discussion:
    http://www.talkchess.com/forum3/viewtopic.php?p=698093#p698093

--- chess/syzygy.py
+++ chess/syzygy.py
@@ -1732,7 +1732,7 @@ class Tablebases(object):
         if wdl > 0:
             # The position is a win or a cursed win by a threat move.
             if success == 3:
-                return 2 if wdl == 2 else 101
+                return 2 if wdl == 2 else 102
 
             # Generate all legal non-capturing pawn moves.
             for move in board.generate_legal_moves(board.pawns, ~board.occupied):
