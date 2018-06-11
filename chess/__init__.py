af89ac9ce009ade0623bc47f3fa645c72c8fa89e
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-06-12 00:46:38 +0200

    Sadly Python 2 will not even parse

--- chess/__init__.py
+++ chess/__init__.py
@@ -35,13 +35,6 @@ import itertools
 import struct
 import sys
 
-if sys.version_info < (3, ):
-    raise ImportError(
-        """You are using python-chess on Python 2.
-
-Python 2 support has been dropped. Consider upgrading to Python 3, or using
-the 0.23.x branch, which will be maintained until the end of 2018.
-""")
 
 COLORS = [WHITE, BLACK] = [True, False]
 COLOR_NAMES = ["black", "white"]
