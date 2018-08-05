552e96c7e28f90bc28929f726584411818578a7a
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-08-05 15:30:59 +0200

    Example: Bishop x-ray attacks

--- examples/xray_attacks.py
+++ examples/xray_attacks.py
@@ -29,9 +29,33 @@ def xray_rook_attackers(board, color, square):
         chess.BB_FILE_ATTACKS[square][file_pieces]))
 
 
+def xray_bishop_attackers(board, color, square):
+    occupied = board.occupied
+    diag_pieces = chess.BB_DIAG_MASKS[square] & occupied
+
+    # Find the closest piece for each direction. These may block attacks.
+    blockers = chess.BB_DIAG_ATTACKS[square][diag_pieces]
+
+    # Only consider blocking pieces of the victim that are more valuable
+    # than bishops.
+    blockers &= board.occupied_co[not color] & (board.rooks | board.queens | board.kings)
+
+    # Now just ignore those blocking pieces.
+    occupied ^= blockers
+
+    # And compute rook attacks.
+    diag_pieces = chess.BB_DIAG_MASKS[square] & occupied
+    return chess.SquareSet(board.occupied_co[color] & board.bishops & chess.BB_DIAG_ATTACKS[square][diag_pieces])
+
+
 def example():
     board = chess.Board("r3k2r/pp3p2/4p2Q/4q1p1/4P3/P2PK3/6PP/R3R3 w q - 1 2")
+    print("rook x-ray, black, h3:")
     print(xray_rook_attackers(board, chess.BLACK, chess.H3))
+
+    board = chess.Board("r1b1r1k1/pp1n1pbp/1qp3p1/B2p4/3P4/Q3PN2/PP2BPPP/R4RK1 b - - 0 1")
+    print("bishop x-ray, white, d8:")
+    print(xray_bishop_attackers(board, chess.WHITE, chess.D8))
 
 
 if __name__ == "__main__":
