a8b0185eced8cfae97fa6a3f779d72111de1a079
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-12-18 18:11:09 +0100

    establish engine connection

--- chess/engine.py
+++ chess/engine.py
@@ -1,8 +1,25 @@
 import asyncio
+import logging
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 class UciProtocol(asyncio.SubprocessProtocol):
-    pass
+    def __init__(self):
+        self.loop = asyncio.get_running_loop()
+        self.transport = None
+
+        self.stdout_buffer = bytearray()
+        self.stderr_buffer = bytearray()
+
+    def connection_made(self, transport):
+        self.transport = transport
+        LOGGER.debug("%s: Connection made", self)
+
+    def __repr__(self):
+        pid = self.transport.get_pid() if self.transport is not None else None
+        return "<{} at {} (pid={})>".format(type(self).__name__, hex(id(self)), pid)
 
 
 async def popen_uci(cmd):
@@ -16,6 +33,7 @@ async def main():
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(main())
