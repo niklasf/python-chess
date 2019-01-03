2f77810e62ebae22c836a01dd3f56a595b1fcde7
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-01-03 17:28:15 +0100

    let sphinx mark coroutines

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import asyncio
 import sys
 import os
+
+import sphinx
+import sphinx.ext.autodoc
+import sphinx.domains.python
 
 # Import the chess module.
 sys.path.insert(0, os.path.abspath('..'))
@@ -35,3 +40,46 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
 html_theme = "default"
+
+# Mark coroutine functions and methods.
+def setup(app):
+    app.add_directive_to_domain("py", "coroutine", PyCoroutineFunction)
+    app.add_directive_to_domain("py", "coroutinemethod", PyCoroutineMethod)
+    app.add_autodocumenter(FunctionDocumenter)
+    app.add_autodocumenter(MethodDocumenter)
+
+    return {
+        "version": "1.0",
+        "parallel_read_safe": True,
+    }
+
+class PyCoroutineMixin:
+    def handle_signature(self, sig, signode):
+        ret = super().handle_signature(sig, signode)
+        signode.insert(0, sphinx.addnodes.desc_annotation("coroutine ", "coroutine "))
+        print("inserted async!")
+        return ret
+
+class PyCoroutineFunction(PyCoroutineMixin, sphinx.domains.python.PyModulelevel):
+    def run(self):
+        self.name = "py:function"
+        return super().run()
+
+class PyCoroutineMethod(PyCoroutineMixin, sphinx.domains.python.PyClassmember):
+    def run(self):
+        self.name = "py:method"
+        return super().run()
+
+class FunctionDocumenter(sphinx.ext.autodoc.FunctionDocumenter):
+    def import_object(self):
+        ret = super().import_object()
+        if ret and asyncio.iscoroutinefunction(self.parent.__dict__.get(self.object_name)):
+            self.directivetype = "coroutine"
+        return ret
+
+class MethodDocumenter(sphinx.ext.autodoc.MethodDocumenter):
+    def import_object(self):
+        ret = super().import_object()
+        if ret and asyncio.iscoroutinefunction(self.parent.__dict__.get(self.object_name)):
+            self.directivetype = "coroutinemethod"
+        return ret
