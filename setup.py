e3445ca9e88b0731ae67d46c3a7be93b90e3b83d
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-01-14 19:07:05 +0100

    Explicitly mention that Python 3.7 is supported

--- setup.py
+++ setup.py
@@ -97,6 +97,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: Games/Entertainment :: Board Games",
         "Topic :: Software Development :: Libraries :: Python Modules",
