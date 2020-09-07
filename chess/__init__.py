6e28b441b57f658064e119221e434363ec0f69b2
Tanmay Garg <40921153+tanmay2004@users.noreply.github.com>
2020-09-07 15:19:30 +0530

    Update __init__.py

--- chess/__init__.py
+++ chess/__init__.py
@@ -143,12 +143,13 @@ def square_name(square: Square) -> str:
     """Gets the name of the square, like ``a3``."""
     return SQUARE_NAMES[square]
 
-def parse_square(name: str) -> Square:
+def parse_square(sq_name: str) -> Square:
     """
-    Gets a square number from the square's name ('e4' returns 28)
-    Returns -1 if the given square name is invalid (like 'd9')
+    Gets a square number from the square's name (e.g., "e4" returns 28).
+    
+    :raises: :exc:`ValueError` if sq_name is invalid (e.g., "d9").
     """
-    return SQUARE_NAMES.find(name)
+    return SQUARE_NAMES.index(sq_name)
 
 def square_distance(a: Square, b: Square) -> int:
     """
