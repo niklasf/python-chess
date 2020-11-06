956166e06356ea7a5e9d5338a3247b9178b2858d
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-11-06 21:43:10 +0100

    Try to fix setup-windows-latest.sh

--- .github/workflows/setup-windows-latest.sh
+++ .github/workflows/setup-windows-latest.sh
@@ -1,6 +1,12 @@
 #!/bin/sh -e
 
+echo Download ...
+choco install wget
 wget https://stockfishchess.org/files/stockfish-11-win.zip
-unzip stockfish-11-win.zip
-mv stockfish-11-win/Windows/stockfish_20011801_x64_bmi2.exe stockfish.exe
+
+echo Unzip ..
+7z e stockfish-11-win.zip stockfish-11-win/Windows/*.exe
+
+echo Setup path ...
+mv stockfish_20011801_x64_bmi2.exe stockfish.exe
 pwd >> $GITHUB_PATH
