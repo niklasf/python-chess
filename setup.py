c8914c2200fe9a6a886b51fc07cf5607eccad9ee
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-11-07 01:03:53 +0100

    Try testing with Python 3.10 alpha

--- setup.py
+++ setup.py
@@ -103,6 +103,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Games/Entertainment :: Board Games",
         "Topic :: Games/Entertainment :: Turn Based Strategy",
         "Topic :: Software Development :: Libraries :: Python Modules",
