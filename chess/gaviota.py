1cca86ff8fcf6ee587d9a138fd774d1b762ed0e6
Niklas Fiekas <niklas.fiekas@backscattering.de>
2023-07-27 19:39:46 +0200

    Remove redundant license headers

--- chess/gaviota.py
+++ chess/gaviota.py
@@ -1,20 +1,3 @@
-# This file is part of the python-chess library.
-# Copyright (C) 2015 Jean-Noël Avila <jn.avila@free.fr>
-# Copyright (C) 2015-2021 Niklas Fiekas <niklas.fiekas@backscattering.de>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 from __future__ import annotations
 
 import ctypes
