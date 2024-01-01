8a70a2c90f228826f34f9bf2629de86e750ab796
Niklas Fiekas <niklas.fiekas@backscattering.de>
2024-01-01 12:21:40 +0100

    Try to fix Stockfish zip extract

--- .github/workflows/setup-windows-latest.sh
+++ .github/workflows/setup-windows-latest.sh
@@ -5,7 +5,7 @@ choco install wget
 wget https://github.com/official-stockfish/Stockfish/releases/download/sf_16/stockfish-windows-x86-64-avx2.zip
 
 echo Unzip ..
-7z e stockfish-windows-x86-64-avx2.zip /stockfish/stockfish-windows-x86-64-avx2.exe
+7z e stockfish-windows-x86-64-avx2.zip stockfish/stockfish-windows-x86-64-avx2.exe
 
 echo Setup path ...
 mv stockfish-windows-x86-64-avx2.exe stockfish.exe
