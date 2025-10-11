376d603694913a82bcb2efa594972a5fba5804f6
Niklas Fiekas <niklas.fiekas@backscattering.de>
2025-10-11 09:20:54 +0200

    Explicitly support Python 3.14

--- setup.py
+++ setup.py
@@ -84,6 +84,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Topic :: Games/Entertainment :: Board Games",
         "Topic :: Games/Entertainment :: Turn Based Strategy",
         "Topic :: Software Development :: Libraries :: Python Modules",
